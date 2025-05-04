e27102d13313a37697b8a6d20302e58f2b31e027
Andrew Kurtz <akurtz@olin.edu>
2025-05-04 18:34:32 -0400

    Updated benchmarking to add inverse and readme

--- src/benchmark.c
+++ src/benchmark.c
@@ -106,6 +106,20 @@ int main(void) {
   printf("  Average GPU time: %f sec\n", gpu_avg_time);
   printf("  Speedup: %fx\n\n", cpu_avg_time / gpu_avg_time);
 
+  // Inverse on CPU
+  puts("CPU inverse time:");
+  cpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* result = cpu_matrix_inverse(square_mat);
+    double end_time = get_current_time();
+    cpu_total_time += end_time - start_time;
+    free_matrix_host(result);
+  }
+  cpu_avg_time = cpu_total_time / (double)num_runs;
+  printf("  Average CPU inverse time: %f sec\n", cpu_avg_time);
+  puts("  Not implemented on GPU\n");
+
   // Linear Regression
   puts("Linear Regression:");
   cpu_total_time = 0;
@@ -131,6 +145,30 @@ int main(void) {
   gpu_avg_time = gpu_total_time / (double)num_runs;
   printf("  Average GPU time: %f sec\n", gpu_avg_time);
   printf("  Speedup: %fx\n\n", cpu_avg_time / gpu_avg_time);
+
+  // Matrix Transfer Time
+  puts("Matrix transfer time:");
+  cpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* d_result = copy_matrix_host_to_device(square_mat);
+    free_matrix_device(d_result);
+    double end_time = get_current_time();
+    cpu_total_time += end_time - start_time;
+  }
+  cpu_avg_time = cpu_total_time / (double)num_runs;
+  printf("  Average CPU to GPU time: %f sec\n", cpu_avg_time);
+
+  gpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* result = copy_matrix_device_to_host(d_square_mat);
+    free_matrix_host(result);
+    double end_time = get_current_time();
+    gpu_total_time += end_time - start_time;
+  }
+  gpu_avg_time = gpu_total_time / (double)num_runs;
+  printf("  Average GPU to CPU time: %f sec\n", gpu_avg_time);
 
   free_matrix_host(square_mat);
   free_matrix_host(col_mat);
