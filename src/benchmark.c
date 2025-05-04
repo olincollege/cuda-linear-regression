8098c8aaaa56dbb0bc4651e171b69cec0b0cc7cb
Andrew Kurtz <akurtz@olin.edu>
2025-05-04 16:18:20 -0400

    Split main into two files

---
+++ src/benchmark.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+
+#include "cpu_matrix.h"
+#include "gpu_matrix.h"
+#include "linear_regression.h"
+#include "matrix.h"
+#include "timing.h"
+
+const size_t num_runs = 20;
+
+int main(void) {
+  // Read benchmarking data matrices
+  puts("Reading data matrices...");
+  Matrix* square_mat = create_matrix_from_csv("../../data/random_300x300.csv");
+  Matrix* d_square_mat = copy_matrix_host_to_device(square_mat);
+
+  Matrix* col_mat = create_matrix_from_csv("../../data/random_300x1.csv");
+  Matrix* d_col_mat = copy_matrix_host_to_device(col_mat);
+
+  puts("Warming up GPU...");
+  // Warm up GPU
+  for (size_t i = 0; i < num_runs; i++) {
+    Matrix* d_weights = gpu_regression(d_square_mat, d_col_mat,
+                                       square_mat->rows, square_mat->cols);
+    free_matrix_device(d_weights);
+  }
+
+  // Benchmarking
+  puts("Starting benchmarking...\n");
+
+  // Matrix Multiplication
+  puts("Matrix Multiplication:");
+  double cpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* result = cpu_matrix_multiply(square_mat, square_mat);
+    double end_time = get_current_time();
+    cpu_total_time += end_time - start_time;
+    free_matrix_host(result);
+  }
+  double cpu_avg_time = cpu_total_time / (double)num_runs;
+  printf("  Average CPU time: %f sec\n", cpu_avg_time);
+
+  double gpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* d_result = gpu_matrix_multiply(d_square_mat, d_square_mat,
+                                           square_mat->rows, square_mat->cols);
+    double end_time = get_current_time();
+    gpu_total_time += end_time - start_time;
+    free_matrix_device(d_result);
+  }
+  double gpu_avg_time = gpu_total_time / (double)num_runs;
+  printf("  Average GPU time: %f sec\n", gpu_avg_time);
+  printf("  Speedup: %fx\n\n", cpu_avg_time / gpu_avg_time);
+
+  // Matrix Transpose
+  puts("Matrix Transpose:");
+  cpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* result = cpu_matrix_transpose(square_mat);
+    double end_time = get_current_time();
+    cpu_total_time += end_time - start_time;
+    free_matrix_host(result);
+  }
+  cpu_avg_time = cpu_total_time / (double)num_runs;
+  printf("  Average CPU time: %f sec\n", cpu_avg_time);
+
+  gpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* d_result =
+        gpu_matrix_transpose(d_square_mat, square_mat->cols, square_mat->rows);
+    double end_time = get_current_time();
+    gpu_total_time += end_time - start_time;
+    free_matrix_device(d_result);
+  }
+  gpu_avg_time = gpu_total_time / (double)num_runs;
+  printf("  Average GPU time: %f sec\n", gpu_avg_time);
+  printf("  Speedup: %fx\n\n", cpu_avg_time / gpu_avg_time);
+
+  // Element-wise Operation
+  puts("Elementwise Operation (adding):");
+  cpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* result = cpu_matrix_add(square_mat, square_mat);
+    double end_time = get_current_time();
+    cpu_total_time += end_time - start_time;
+    free_matrix_host(result);
+  }
+  cpu_avg_time = cpu_total_time / (double)num_runs;
+  printf("  Average CPU time: %f sec\n", cpu_avg_time);
+
+  gpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* d_result = gpu_matrix_add(d_square_mat, d_square_mat,
+                                      square_mat->rows, square_mat->cols);
+    double end_time = get_current_time();
+    gpu_total_time += end_time - start_time;
+    free_matrix_device(d_result);
+  }
+  gpu_avg_time = gpu_total_time / (double)num_runs;
+  printf("  Average GPU time: %f sec\n", gpu_avg_time);
+  printf("  Speedup: %fx\n\n", cpu_avg_time / gpu_avg_time);
+
+  // Linear Regression
+  puts("Linear Regression:");
+  cpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* result = cpu_regression(square_mat, col_mat);
+    double end_time = get_current_time();
+    cpu_total_time += end_time - start_time;
+    free_matrix_host(result);
+  }
+  cpu_avg_time = cpu_total_time / (double)num_runs;
+  printf("  Average CPU time: %f sec\n", cpu_avg_time);
+
+  gpu_total_time = 0;
+  for (size_t i = 0; i < num_runs; i++) {
+    double start_time = get_current_time();
+    Matrix* d_result = gpu_regression(d_square_mat, d_col_mat, square_mat->rows,
+                                      square_mat->cols);
+    double end_time = get_current_time();
+    gpu_total_time += end_time - start_time;
+    free_matrix_device(d_result);
+  }
+  gpu_avg_time = gpu_total_time / (double)num_runs;
+  printf("  Average GPU time: %f sec\n", gpu_avg_time);
+  printf("  Speedup: %fx\n\n", cpu_avg_time / gpu_avg_time);
+
+  free_matrix_host(square_mat);
+  free_matrix_host(col_mat);
+  free_matrix_device(d_square_mat);
+  free_matrix_device(d_col_mat);
+  return 0;
+}
--- src/regression.c
+++ src/regression.c
@@ -3,9 +3,7 @@
 #include "cpu_matrix.h"
 #include "linear_regression.h"
 #include "matrix.h"
-#include "timing.h"
 
-const size_t num_runs = 100;
 char* const weight_meaning[] = {"Gestation length (days)",
                                 "If not first pregnancy",
                                 "Mother's age (yrs)",
@@ -16,48 +14,14 @@ char* const weight_meaning[] = {"Gestation length (days)",
 
 int main(void) {
   // Read data matrices
+  puts("Reading data matrices...");
   Matrix* X_train = create_matrix_from_csv("../../data/X_train.csv");
   Matrix* y_train = create_matrix_from_csv("../../data/y_train.csv");
   Matrix* X_test = create_matrix_from_csv("../../data/X_test.csv");
   Matrix* y_test = create_matrix_from_csv("../../data/y_test.csv");
 
-  // Benchmark CPU
-  double cpu_total_time = 0;
-  for (size_t i = 0; i < num_runs; i++) {
-    double start_time = get_current_time();
-    Matrix* weights = cpu_regression(X_train, y_train);
-    double end_time = get_current_time();
-    cpu_total_time += end_time - start_time;
-    free_matrix_host(weights);
-  }
-  double cpu_avg_time = cpu_total_time / (double)num_runs;
-  printf("Average CPU time: %f\n", cpu_avg_time);
-
-  // Benchmark GPU
-  Matrix* d_X_train = copy_matrix_host_to_device(X_train);
-  Matrix* d_y_train = copy_matrix_host_to_device(y_train);
-
-  // Warm up
-  for (size_t i = 0; i < num_runs; i++) {
-    Matrix* d_weights =
-        gpu_regression(d_X_train, d_y_train, X_train->rows, X_train->cols);
-    free_matrix_device(d_weights);
-  }
-
-  // Real GPU trial
-  double gpu_total_time = 0;
-  for (size_t i = 0; i < num_runs; i++) {
-    double start_time = get_current_time();
-    Matrix* d_weights =
-        gpu_regression(d_X_train, d_y_train, X_train->rows, X_train->cols);
-    double end_time = get_current_time();
-    gpu_total_time += end_time - start_time;
-    free_matrix_device(d_weights);
-  }
-  double gpu_avg_time = gpu_total_time / (double)num_runs;
-  printf("Average GPU time: %f\n", gpu_avg_time);
-
   // Calculate weights to use
+  puts("Running linear regression...");
   Matrix* weights = cpu_regression(X_train, y_train);
 
   // Print weights
@@ -83,7 +47,5 @@ int main(void) {
   free_matrix_host(y_train);
   free_matrix_host(X_test);
   free_matrix_host(y_test);
-  free_matrix_device(d_X_train);
-  free_matrix_device(d_y_train);
   return 0;
 }
